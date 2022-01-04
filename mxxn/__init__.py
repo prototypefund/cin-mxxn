823b3d1324c1c6b910d7459430143d03f613b9be
lluar <lluar@ccodein.org>
2022-01-04 19:47:13 +0100

    MOD: rename of the package to mxxn

--- mxxn/__init__.py
+++ mxxn/__init__.py
@@ -4,9 +4,9 @@ The Mixxin framework package.
 Mixxin is a Falcon, SQLAlchemy, and TypeScript-based
 framework for rapid web application development.
 The package offers an easy-to-use plugin interface based
-on Python packages in virtual Python environments. Plugins
+on Pytho packages in virtual Python environments. Plugins
 are called mixxins and they are normal Python packages.
 """
-from mixxin.app import App
+from mxxn.app import App
 
 app = App()

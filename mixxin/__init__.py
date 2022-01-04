bc01ef1788ceca766c4128ee38b859a3545e0179
lluar <lluar@ccodein.org>
2022-01-04 19:29:21 +0100

    ADD: the env module

--- mixxin/__init__.py
+++ mixxin/__init__.py
@@ -1,1 +1,9 @@
- 
+"""
+The Mixxin framework package.
+
+Mixxin is a Falcon, SQLAlchemy, and TypeScript-based
+framework for rapid web application development.
+The package offers an easy-to-use plugin interface based
+on Python packages in virtual Python environments. Plugins
+are called mixxins and they are normal Python packages.
+"""
---
+++ mixxin/env.py
@@ -0,0 +1,9 @@
+"""
+Module for using components of the mixxin environment.
+
+The mixxin environment has the three basic package types
+mixxin, mixxins and app. These packages have essentially
+the same structure. On the basis of this structure, when the
+application starts, elements are automatically loaded from
+the packages and registered in the framework.
+"""

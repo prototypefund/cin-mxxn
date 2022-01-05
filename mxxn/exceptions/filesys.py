05aa0906effee04605b623650441f495f1df986f
lluar <lluar@ccodein.org>
2022-01-05 13:42:18 +0100

    ADD: filesys exceptions module

---
+++ mxxn/exceptions/filesys.py
@@ -0,0 +1,25 @@
+"""
+The filesystem exceptions module.
+
+This module contains exceptions that can be
+thrown when accessing files or directories.
+"""
+from mixxin import exceptions
+
+
+class FileSysError(exceptions.Base):
+    """The base exception for all filesystem exceptions."""
+
+    pass
+
+
+class FileNotExistError(FileSysError):
+    """Raised if the file does not exist."""
+
+    pass
+
+
+class PathNotExistError(FileSysError):
+    """Raised if the path does not exist."""
+
+    pass

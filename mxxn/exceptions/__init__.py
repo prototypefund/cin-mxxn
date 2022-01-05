8ecdc0ac25abc694ab1a160841817d2fdab18b88
lluar <lluar@ccodein.org>
2022-01-05 13:39:50 +0100

    ADD: exceptions package

---
+++ mxxn/exceptions/__init__.py
@@ -0,0 +1,12 @@
+"""
+The the exception package.
+
+In this module the base class of all framework exceptions
+is defined.
+"""
+
+
+class Base(Exception):
+    """The base class for all framework exceptions."""
+
+    pass

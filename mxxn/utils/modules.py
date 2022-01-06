505c1beb9701270cd3a326975749ebfec90534d0
lluar <lluar@ccodein.org>
2022-01-06 20:14:31 +0100

    ADD: utils for modules

---
+++ mxxn/utils/modules.py
@@ -0,0 +1,89 @@
+"""This module provides tools for working with modules."""
+import pkgutil
+import importlib
+import inspect
+from typing import List, Type
+
+
+def submodules(module_name: str, modules: List[str]) -> None:
+    """
+    Get all submodules of a module.
+
+    The function searches the given module recursively,
+    including subpackages.
+
+    Args:
+        module_name: The name of the module.
+        modules: The list in which the found modules are stored.
+
+    Raises:
+        ModuleNotFoundError: If the given module does not exist.
+    """
+    try:
+        module = importlib.import_module(module_name)
+        submodules_list = list(
+            pkgutil.iter_modules(module.__path__)  # type: ignore
+        )
+
+    except ModuleNotFoundError as e:
+        raise ModuleNotFoundError(
+            'The module {} for which the submodules are searched does '
+            'not exist.'.format(module_name)
+        ) from e
+
+    except AttributeError:
+        return
+
+    for submodule in submodules_list:
+        name = module_name + '.' + submodule.name
+        if submodule.ispkg:
+            submodules(name, modules)
+            modules.append(name)
+        else:
+            modules.append(name)
+
+
+def classes(module_name: str) -> List[Type]:
+    """
+    Get all classes of a module.
+
+    Args:
+        module_name: The name of the module.
+
+    Return:
+        A list of classes.
+
+    Raises:
+        ModuleNotFoundError: If the passed module does not exist.
+    """
+    classes_list = []
+    module = importlib.import_module(module_name)
+
+    for name, obj in inspect.getmembers(module):
+        if inspect.isclass(obj):
+            classes_list.append(obj)
+
+    return classes_list
+
+
+def classes_recursively(module_name: str) -> List[Type]:
+    """
+    Get all classes of the module and submodules recursively.
+
+    Args:
+        module_name: The name of the module.
+
+    Return:
+        A List of classes.
+
+    Raises:
+        ModuleNotFoundError: If the passed module does not exist.
+    """
+    submodules_list: List[str] = []
+    classes_list = classes(module_name)
+    submodules(module_name, submodules_list)
+
+    for submodule in submodules_list:
+        classes_list.extend(classes(submodule))
+
+    return classes_list
---
+++ tests/utils/test_modules.py
@@ -0,0 +1,89 @@
+"""Test for the utils.modules module."""
+import pytest
+import importlib
+import inspect
+from mxxn.utils.modules import submodules, classes, classes_recursively
+
+
+@pytest.fixture()
+def modules_tree(mixxin_env):
+    """Create a test directory with some Python modules."""
+    content = """
+        class Test1(object):
+            pass
+
+        class Test2(object):
+            pass
+    """
+    (mixxin_env/'mxnone/__init__.py').write_text(inspect.cleandoc(content))
+    (mixxin_env/'mxnone/module_1.py').write_text(inspect.cleandoc(content))
+    (mixxin_env/'mxnone/module_2.py').touch()
+    (mixxin_env/'mxnone/subpackage').mkdir()
+    (mixxin_env/'mxnone/subpackage/__init__.py').touch()
+    (mixxin_env/'mxnone/subpackage/module_3.py').write_text(
+        inspect.cleandoc(content)
+    )
+    (mixxin_env/'mxnone/subpackage/module_4.py').touch()
+
+    return mixxin_env
+
+
+class TestSubmodules(object):
+    """Test for submodule function."""
+
+    def test_if_all_module_found(self, modules_tree):
+        """Test if all modules were found."""
+        modules = []
+
+        submodules('mxnone', modules)
+
+        assert modules == [
+            'mxnone.module_1',
+            'mxnone.module_2',
+            'mxnone.subpackage.module_3',
+            'mxnone.subpackage.module_4',
+            'mxnone.subpackage'
+        ]
+
+    def test_raise_module_not_found_error(self):
+        """Test if ModuleNotFoundError raised."""
+        modules = []
+
+        with pytest.raises(ModuleNotFoundError):
+            submodules('xxxyyyzzz', modules)
+
+
+class TestCasses(object):
+    """Test for classes function."""
+
+    def test_if_all_classes_found(self, modules_tree):
+        """Test if all classes were found."""
+        classes_list = classes('mxnone')
+
+        assert len(classes_list) == 2
+        assert classes_list[0].__name__ == 'Test1'
+        assert classes_list[1].__name__ == 'Test2'
+
+
+class TestCassesRecursively(object):
+    """Test for classes_recursively function."""
+
+    def test_if_all_classen_found_recursively(self, modules_tree):
+        """Test if all classes were found recursively."""
+        classes_list = classes_recursively('mxnone')
+
+        assert len(classes_list) == 6
+        assert classes_list[0].__module__ == 'mxnone'
+        assert classes_list[0].__name__ == 'Test1'
+        assert classes_list[1].__module__ == 'mxnone'
+        assert classes_list[1].__name__ == 'Test2'
+        assert classes_list[2].__module__ == 'mxnone.module_1'
+        assert classes_list[2].__name__ == 'Test1'
+        assert classes_list[3].__module__ == 'mxnone.module_1'
+        assert classes_list[3].__name__ == 'Test2'
+        assert classes_list[4].__module__ == \
+            'mxnone.subpackage.module_3'
+        assert classes_list[4].__name__ == 'Test1'
+        assert classes_list[5].__module__ == \
+            'mxnone.subpackage.module_3'
+        assert classes_list[5].__name__ == 'Test2'

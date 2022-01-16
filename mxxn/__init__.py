56f571401066b5c4ccc1cd0767743bda74bff1b4
lluar <lluar@ccodein.org>
2022-01-16 16:38:43 +0100

    MOD: app module renamed to application

--- mxxn/__init__.py
+++ mxxn/__init__.py
@@ -4,9 +4,9 @@ The Mixxin framework package.
 Mixxin is a Falcon, SQLAlchemy, and TypeScript-based
 framework for rapid web application development.
 The package offers an easy-to-use plugin interface based
-on Pytho packages in virtual Python environments. Plugins
-are called mixxins and they are normal Python packages.
+on Python packages in virtual Python environments. Plugins
+are called mixins and they are normal Python packages.
 """
-from mxxn.app import App
+from mxxn.application import App
 
 app = App()
---
+++ mxxn/application.py
@@ -0,0 +1,107 @@
+"""The app module."""
+from falcon import asgi
+from mxxn.settings import Settings
+from mxxn.logging import logger
+from mxxn.env import Mixxin, mixins, Mixin, MixxinApp
+from mxxn.exceptions import env as env_ex
+
+
+class App(object):
+    """The App class is the base for creating a Mixxin applications."""
+
+    def __init__(self) -> None:
+        self.settings = Settings()
+        self.asgi = asgi.App()
+        self._register_resources()
+
+    def _register_resources(self) -> None:
+        """
+        Register all resources.
+
+        This method registers all resources available in the mixxin and
+        in installed mixins.
+
+        """
+        log = logger('registration')
+        mixxin = Mixxin()
+
+        for resource_dict in mixxin.resources:
+            resource = resource_dict['resource']
+            routes = resource_dict['routes']
+
+            for route in routes:
+                if len(route) > 1:
+                    self.asgi.add_route(route[0], resource(), suffix=route[1])
+
+                    continue
+
+                if route[0] == '/.root':
+                    self.asgi.add_route('/', resource())
+
+                    continue
+
+                self.asgi.add_route(route[0], resource())
+
+        log.debug(
+            'The resources of the mixxin package were registered.'
+        )
+
+        for mixin_name in mixins(self.settings):
+            mixin = Mixin(mixin_name)
+
+            if not mixin.resources:
+                log.debug(
+                    'The mixin "{}" contains no resources.'
+                    .format(mixin_name)
+                )
+
+                continue
+
+            for resource_dict in mixin.resources:
+                resource = resource_dict['resource']
+                routes = resource_dict['routes']
+
+                for route in routes:
+                    prefix = '/mixins/' + mixin_name
+                    if len(route) > 1:
+                        self.asgi.add_route(
+                            prefix + route[0], resource(),
+                            suffix=route[1]
+                        )
+
+                        continue
+
+                    self.asgi.add_route(prefix + route[0], resource())
+
+            log.debug(
+                'The resources of the mixin "{}" were registered.'
+                .format(mixin_name)
+            )
+
+        try:
+            app = MixxinApp()
+
+            resource_list = app.resources()
+
+            for resource_dict in resource_list:
+                resource = resource_dict['resource']
+                routes = resource_dict['routes']
+
+                for route in routes:
+                    if len(route) > 1:
+                        self.asgi.add_route(
+                            '/app'+route[0],
+                            resource(),
+                            suffix=route[1]
+                        )
+
+                        continue
+
+                    self.asgi.add_route('/app'+route[0], resource())
+
+            log.debug(
+                'The resources of the application package were registered.'
+            )
+
+        except env_ex.MixxinAppNotExistError:
+            pass

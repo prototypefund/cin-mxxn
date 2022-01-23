8fc24b4dff2dbb5d8d752b686cf45bad669acb33
lluar <lluar@ccodein.org>
2022-01-23 16:10:00 +0100

    FIX: type annotation error in resources package

--- mxxn/resources/__init__.py
+++ mxxn/resources/__init__.py
@@ -1,23 +1,24 @@
 """The Resource package of the Mixxin package."""
 import falcon
+from falcon import Request, Response
 from mxxn.hooks import render
 from mxxn.exceptions import env as env_ex
 from mxxn import env
 
 
-class Root(object):
+class Root():
     """The Root resource of the Mxxn package."""
 
-    async def on_get(self, req, resp):
-        """Forward the get request to the app resource."""
+    async def on_get(self, req: Request, resp: Response) -> None:
+        """Forward the get request to the App resource."""
         raise falcon.HTTPMovedPermanently('.app')
 
 
-class App(object):
+class App():
     """The App resource of the Mxxn package."""
 
     @falcon.after(render, template='app.j2')
-    async def on_get(self, req, resp):
+    async def on_get(self, req: Request, resp: Response) -> None:
         """Get the application frontend."""
         js_urls = []
         mxxn = env.Mxxn()
@@ -36,7 +37,7 @@ class App(object):
             mxnapp = env.MxnApp()
 
             for js_file in mxnapp.js_files:
-                js_urls.append('static/app/js/'+js_file)
+                js_urls.append('static/app/js'/js_file)
 
         except env_ex.MxnAppNotExistError:
             pass

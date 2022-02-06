47a2485e4cb22b34d5d078cebd2b04c3ca6c6790
lluar <lluar@ccodein.org>
2022-02-06 15:59:17 +0100

    ADD: inital checks to the ConfigDir

--- mxxn/config.py
+++ mxxn/config.py
@@ -29,9 +29,44 @@ class ConfigDir(object):
         Args:
             path: The relative path to the configuration files.
 
+        Raises:
+            mxxn.exceptions.filesys.PathNotExistError:
+                If the path does not exist.
+            mxxn.exceptions.filesys.ExtensionError:
+                If there is a file its extension is not ".json".
+            mxxn.exceptions.config.NoDefaultConfigError:
+                If there is no default configuration in the folder.
+            mxxn.exceptions.config.TooManyDefaultConfigsError:
+                If there are too many default configurations in the folder.
+
         """
+        if not path.is_dir():
+            raise filesys_ex.PathNotExistError(
+                'The config path {} does not exist.'.format(path))
+
         self._path = path
 
+        default_count = 0
+
+        for file in self.files:
+            if not file.name.endswith('.json'):
+                raise filesys_ex.ExtensionError(
+                    'The extension of a config file "{}"'
+                    'is not .json'.format(file)
+                )
+
+            if file.name.endswith('-default.json'):
+                default_count += 1
+
+        if default_count == 0:
+            raise config_ex.NoDefaultConfigError(
+                'There is no default config file.'
+            )
+
+        if default_count > 1:
+            raise config_ex.TooManyDefaultConfigsError(
+                'There are too many default config files.'
+            )
 
     @property
     def files(self) -> dict:
@@ -42,6 +77,6 @@ class ConfigDir(object):
             list: A list of files in the directory.
 
         """
-        files = [x.name for x in self._path.glob('*') if x.is_file()]
+        files = [x for x in self._path.glob('*') if x.is_file()]
 
         return files
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,49 @@
 """Tests for the config module."""
 import pytest
 from mxxn.exceptions import filesys as filesys_ex
+from mxxn.exceptions import config as config_ex
 from mxxn import config
 
+
+class TestConfigDirInit(object):
+    """Test for the initialization of ConfigDir."""
+
+    def test_path_not_exist(self, mxxn_env):
+        """Is PathNotExistError is raised if path not exist."""
+
+        with pytest.raises(filesys_ex.PathNotExistError):
+            config.ConfigDir(mxxn_env/'mxnone/xxxyyyzzz')
+
+    def test_non_json_extension(self, mxxn_env):
+        """Is ExtensionError is raised."""
+        mxnone_config_dir = mxxn_env/'mxnone/config'
+        mxnone_config_dir.mkdir()
+        (mxnone_config_dir/'de-DE.json').touch()
+        (mxnone_config_dir/'en-default.json').touch()
+        (mxnone_config_dir/'fake.txt').touch()
+
+        with pytest.raises(filesys_ex.ExtensionError):
+            config.ConfigDir(mxnone_config_dir)
+
+    def test_too_many_defaults(self, mxxn_env):
+        mxnone_config_dir = mxxn_env/'mxnone/config'
+        mxnone_config_dir.mkdir()
+        (mxnone_config_dir/'de-DE.json').touch()
+        (mxnone_config_dir/'en-default.json').touch()
+        (mxnone_config_dir/'de-default.json').touch()
+
+        with pytest.raises(config_ex.TooManyDefaultConfigsError):
+            config.ConfigDir(mxnone_config_dir)
+
+    def test_no_default(self, mxxn_env):
+        """Test if NoDefaultConfigError is raised."""
+        mxnone_config_dir = mxxn_env/'mxnone/config'
+        mxnone_config_dir.mkdir()
+        (mxnone_config_dir/'de-DE.json').touch()
+        (mxnone_config_dir/'en.json').touch()
+
+        with pytest.raises(config_ex.NoDefaultConfigError):
+            config.ConfigDir(mxnone_config_dir)
 
 
 class TestConfigDirFiles():
@@ -12,9 +53,11 @@ class TestConfigDirFiles():
         """Are the filenames returned."""
         mxnone_config_dir = mxxn_env/'mxnone/config'
         mxnone_config_dir.mkdir()
-        (mxnone_config_dir/'de-DE.json').touch()
-        (mxnone_config_dir/'en-default.json').touch()
+        de_file = mxnone_config_dir/'de-DE.json'
+        de_file.touch()
+        en_file = mxnone_config_dir/'en-default.json'
+        en_file.touch()
 
         config_dir = config.ConfigDir(mxnone_config_dir)
 
-        assert config_dir.files == ['en-default.json', 'de-DE.json']
+        assert config_dir.files == [en_file, de_file]

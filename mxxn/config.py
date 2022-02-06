a2e525981402e4bd4bf6bae3a9adcc39e0598bb8
lluar <lluar@ccodein.org>
2022-02-06 15:32:07 +0100

    ADD: files property to the ConfigDir class

--- mxxn/config.py
+++ mxxn/config.py
@@ -1,1 +1,47 @@
 """This module provides functionality to work with configuration files."""
+from pathlib import Path
+from mxxn.exceptions import config as config_ex
+from mxxn.exceptions import filesys as filesys_ex
+
+
+class ConfigDir(object):
+    """
+    This class abstracts a configuration directory.
+
+    A configuration directory is a directory containing JSON files for
+    different setups (for example, for themes or strings). The respective
+    configuration file can be selected and its content returned as a
+    dictionary. Each configuration folder must contain a default
+    configuration file. This file will be used as a base and will be
+    overwritten with the contents of the selected file. Only JSON files
+    are allowed in a configuration directory. In addition, the folder
+    must contain a file with the extension "-default.json".
+    """
+
+    def __init__(self, path: Path) -> None:
+        """
+        Initialize the ConfigDir object.
+
+        The constructor checks whether the path to the configuration files
+        exists, if there are only JSON files in it, and if there is a
+        default file in it.
+
+        Args:
+            path: The relative path to the configuration files.
+
+        """
+        self._path = path
+
+
+    @property
+    def files(self) -> dict:
+        """
+        Get the names of the files in the directory.
+
+        Returns:
+            list: A list of files in the directory.
+
+        """
+        files = [x.name for x in self._path.glob('*') if x.is_file()]
+
+        return files
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,1 +1,20 @@
 """Tests for the config module."""
+import pytest
+from mxxn.exceptions import filesys as filesys_ex
+from mxxn import config
+
+
+
+class TestConfigDirFiles():
+    """Tests for the files method of ConfigDir class."""
+
+    def test_files_returned(self, mxxn_env):
+        """Are the filenames returned."""
+        mxnone_config_dir = mxxn_env/'mxnone/config'
+        mxnone_config_dir.mkdir()
+        (mxnone_config_dir/'de-DE.json').touch()
+        (mxnone_config_dir/'en-default.json').touch()
+
+        config_dir = config.ConfigDir(mxnone_config_dir)
+
+        assert config_dir.files == ['en-default.json', 'de-DE.json']

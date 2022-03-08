fce1ac6c829003c30c48b44bfbb31ebdbd1b0f08
lluar <lluar@ccodein.org>
2022-03-08 19:16:45 +0100

    MOD: code refactoring

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,11 +1,11 @@
-
 """This module contains tests for the app module."""
 from inspect import cleandoc
 import pytest
 from unittest.mock import patch, PropertyMock
 import falcon
-from falcon import testing
+from falcon.testing import TestClient
 from mxxn.application import App
+from mxxn import env
 
 
 @pytest.fixture()
@@ -145,7 +145,7 @@ class TestAppRegisterResources():
     def test_mxxn_routes_added(self, resources):
         """All routes of the mxxn were added."""
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
 
         response_root = client.simulate_get('/')
 
@@ -172,7 +172,7 @@ class TestAppRegisterResources():
     def test_mxn_routes_added(self, resources):
         """All routes of the mxns were added."""
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
 
         mxnone_response_one = client.simulate_get('/app/mxns/one')
         mxnone_response_two = client.simulate_get('/app/mxns/one/resourcetwo')
@@ -213,7 +213,7 @@ class TestAppRegisterResources():
     def test_mxnapp_routes_added(self, resources):
         """All routes of the mixxin application were added."""
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
 
         response_one = client.simulate_get('/app/mxnapp')
         response_two = client.simulate_get('/app/mxnapp/resourcetwo')
@@ -259,7 +259,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_root = client.simulate_get('/')
         response_one = client.simulate_get('/app/resourceone')
         response_two = client.simulate_get('/app/resourcetwo')
@@ -310,7 +310,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_root = client.simulate_get('/')
         response_one = client.simulate_get('/app/resourceone')
         response_two = client.simulate_get('/app/resourcetwo')
@@ -361,7 +361,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_root = client.simulate_get('/')
         response_one = client.simulate_get('/app/resourceone')
         response_two = client.simulate_get('/app/resourcetwo')
@@ -411,7 +411,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_root = client.simulate_get('/')
         response_one = client.simulate_get('/app/resourceone')
         response_two = client.simulate_get('/app/resourcetwo')
@@ -461,7 +461,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_one = client.simulate_get('/app/mxns/one')
         response_two = client.simulate_get('/app/mxns/one/resourcetwo')
         response_two_suffix = client.simulate_get(
@@ -509,7 +509,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_one = client.simulate_get('/app/mxns/one')
         response_two = client.simulate_get('/app/mxns/one/resourcetwo')
         response_two_suffix = client.simulate_get(
@@ -557,7 +557,7 @@ class TestAppRegisterResources():
         )
 
         app = App()
-        client = testing.TestClient(app.asgi)
+        client = TestClient(app.asgi)
         response_one = client.simulate_get('/app/mxns/one')
         response_two = client.simulate_get('/app/mxns/one/resourcetwo')
         response_two_suffix = client.simulate_get(
@@ -579,67 +579,54 @@ class TestAppRegisterResources():
 class TestRegisterStaticPaths():
     """Tests for the _register_static_paths method of the App class."""
 
-    def test_mxn_registration(self, mxxn_env):
+    @pytest.mark.parametrize('mxn_name', ['mxnone', 'mxntwo', 'mxnthree'])
+    def test_mxn_registration(self, mxxn_static_files_env, mxn_name):
         """The static folders have been registered."""
-        mxnone_static_path = mxxn_env/'mxnone/frontend/static'
-        mxntwo_static_path = mxxn_env/'mxntwo/frontend/static'
-        mxnone_static_path.mkdir(parents=True)
-        mxntwo_static_path.mkdir(parents=True)
-
+        mxn = env.Mxn(mxn_name)
         app = App()
 
-        print(app.asgi._static_routes[2][0]._prefix)
+        client = TestClient(app.asgi)
+        result_mxnone_js = client.simulate_get(
+                '/static/mxns/' + mxn.unprefixed_name + '/js/javascript.js')
 
-        assert len(app.asgi._static_routes) == 3
-        assert app.asgi._static_routes[0][0]._prefix == '/static/mxns/two/'
-        assert app.asgi._static_routes[1][0]._prefix == '/static/mxns/one/'
-        assert app.asgi._static_routes[2][0]._prefix == '/static/'
+        result_mxnone_html = client.simulate_get(
+                '/static/mxns/' + mxn.unprefixed_name + '/index.html')
 
-    def test_mxnapp_registration(self, mxxn_env):
+        assert result_mxnone_js.status_code == 200
+        assert result_mxnone_html.status_code == 200
+
+    def test_mxnapp_registration(self, mxxn_static_files_env):
         """The static folder of mxnapp has been registered."""
-        mxnapp = mxxn_env/'mxnapp/frontend/static'
-        mxnapp.mkdir(parents=True)
-
         app = App()
 
-        assert len(app.asgi._static_routes) == 2
-        assert app.asgi._static_routes[0][0]._prefix == '/static/mxnapp/'
-        assert app.asgi._static_routes[1][0]._prefix == '/static/'
+        client = TestClient(app.asgi)
+        result_mxnone_js = client.simulate_get(
+                '/static/mxnapp/js/javascript.js')
 
-    def test_mxxn_covers_added(self, mxxn_env):
+        result_mxnone_html = client.simulate_get(
+                '/static/mxnapp/index.html')
+
+        assert result_mxnone_js.status_code == 200
+        assert result_mxnone_html.status_code == 200
+
+    def test_mxxn_covers_added(self, mxxn_static_file_covers_env):
         """The mxxn covers were added."""
-        (mxxn_env/'mxnapp/covers/mxxn/frontend/static/js').mkdir(parents=True)
-        (mxxn_env/'mxnapp/covers/mxxn/frontend/static/js/mxxn.js').touch()
-        (mxxn_env/'mxnapp/covers/mxxn/frontend/static/index.html').touch()
-
         app = App()
-        client = testing.TestClient(app.asgi)
-        result = client.simulate_get('/static/covers/mxxn/index.html')
+        client = TestClient(app.asgi)
+        result = client.simulate_get('/static/covers/mxxn/js/mxxn.js')
 
         assert result.status_code == 200
+        assert result.text == 'mxxn cover'
 
-    def test_tmp(self, mxxn_env):
-        mxns_cover_path = mxxn_env/'mxnapp/covers/mxns/'
-
-        (mxns_cover_path/'mxnone/frontend/static/js').mkdir(parents=True)
-        (mxns_cover_path/'mxntwo/frontend/static/js').mkdir(parents=True)
-        (mxns_cover_path/'mxnone/frontend/static/js/mxn.js').touch()
-        (mxns_cover_path/'mxnone/frontend/static/index.html').touch()
-        (mxns_cover_path/'mxntwo/frontend/static/js/mxn.js').touch()
-        (mxns_cover_path/'mxntwo/frontend/static/index.html').touch()
+    @pytest.mark.parametrize('mxn_name', ['mxnone', 'mxntwo', 'mxnthree'])
+    def test_mxn_covers_added(self, mxxn_static_file_covers_env, mxn_name):
+        """The mxn covers were added."""
+        mxn = env.Mxn(mxn_name)
 
         app = App()
-        client = testing.TestClient(app.asgi)
-        mxnone_result_index = client.simulate_get(
-                '/static/covers/mxns/one/index.html')
-        mxnone_result_js = client.simulate_get(
-                '/static/covers/mxns/one/js/mxn.js')
-        mxntwo_result_index = client.simulate_get(
-                '/static/covers/mxns/two/index.html')
-        mxntwo_result_js = client.simulate_get(
-                '/static/covers/mxns/two/js/mxn.js')
+        client = TestClient(app.asgi)
+        results = client.simulate_get(
+                '/static/covers/mxns/'+mxn.unprefixed_name+'/js/javascript.js')
 
-        assert mxnone_result_index.status_code == 200
-        assert mxnone_result_js.status_code == 200
-        assert mxntwo_result_index.status_code == 200
-        assert mxntwo_result_js.status_code == 200
+        assert results.status_code == 200
+        assert results.text == mxn_name + ' cover'

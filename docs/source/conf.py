f01c7635a20cfe72b4135f7a0a57e6d5f72b1503
lluar <lluar@ccodein.org>
2022-03-18 15:20:36 +0100

    ADD: [docs] framework logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
 """The config of the docs."""
-project = 'mixxin'
+project = 'mxxn'
 copyright = '2022, lluar'
 author = 'lluar'
 
@@ -46,3 +46,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/logo.svg'
+html_theme_options = {
+    'logo_only': True
+}
+
+

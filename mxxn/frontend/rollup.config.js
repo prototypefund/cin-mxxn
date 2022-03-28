faa9ff4d466833d1d3a45c4fa9a1f1cbf84a09cb
lluar <lluar@ccodein.org>
2022-03-28 16:59:48 +0200

    ADD: [frontend] types to the Icon component

--- mxxn/frontend/rollup.config.js
+++ mxxn/frontend/rollup.config.js
@@ -1,5 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import riot from 'rollup-plugin-riot'
 
 
@@ -11,8 +11,11 @@ export default {
     		format: 'iife'
   	},
 	plugins: [
-        riot(),
-		typescript(),
-		nodeResolve()
+		nodeResolve(),
+		riot(),
+		typescript({
+			include: ['src/**/*.ts+(|x)', 'src/**/*.ts+(|x)', 'src/**/*.riot']
+		}),
 	]
 };
+
--- path.py
+++
@@ -1,7 +0,0 @@
-from os import environ
-from pathlib import Pathjk
-
-print(environ['PATH'])
-
-environ['PATH'] += ':/test'
-print(environ['PATH'])

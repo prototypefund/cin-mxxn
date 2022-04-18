09b41aa7510839a41678056bd8202fb5acf35e60
lluar <lluar@ccodein.org>
2022-04-18 15:25:43 +0200

    ADD: [docs] import path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,12 @@
 import os
 import sys
-sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../mxxn")
-))
+from pathlib import Path
+
+sys.path.insert(0, os.path.abspath(Path(__file__).parents[1]))
+
+print('#############################')
+print(sys.path)
+
 """The config of the docs."""
 project = 'mxxn'
 copyright = '2022, lluar'

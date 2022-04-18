2707931b9790c436df55999d2635fae60f275e55
lluar <lluar@ccodein.org>
2022-04-18 15:43:06 +0200

    FIX: [docs] path error for CI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,10 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-sys.path.insert(0, os.path.abspath(Path(__file__).parents[2]))
-
-print('#############################')
-print(sys.path)
+sys.path.insert(0, str(Path(__file__).parents[2]))
 
 """The config of the docs."""
 project = 'mxxn'

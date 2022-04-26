580a163a21d67a7fb1f6419002b8dcc7c3dc173c
lluar <lluar@ccodein.org>
2022-04-26 19:50:09 +0200

    ADD: [frontend] test for icon update

--- mxxn/frontend/tests/elements/icon.test.ts
+++ mxxn/frontend/tests/elements/icon.test.ts
@@ -26,7 +26,7 @@ describe('Tests for Icon component.',function() {
     await icon.isLoading;
 
     expect(icon.shadowRoot.innerHTML).toContain('11H21V13H3V11M3');
-  })
+  });
 
   it('Wrong initial icon name.', async () => {
     const test = async () => {
@@ -39,7 +39,38 @@ describe('Tests for Icon component.',function() {
     }
 
     await expectAsync(test()).toBeRejectedWithError(IconLoadError);
-  })
+  });
+
+  it('Update the icon.', async () => {
+    const svgOne = `
+      <svg>
+        <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+      </svg>`
+
+    const svgTwo = `
+      <svg>
+        <path d="M19,13H5V11H19V13Z" />
+      </svg>`
+
+    const responseOne = new Response(svgOne , { status: 200, statusText: 'OK', });
+    const responseTwo = new Response(svgTwo , { status: 200, statusText: 'OK', });
+    Icon.request = jasmine.createSpy('request').and.returnValues(responseOne, responseTwo);
+
+    const iconElement = document.createElement('mxxn-icon');
+    iconElement.setAttribute('name', 'menu');
+    document.body.appendChild(iconElement);
+    const icon = document.body.querySelector<Icon>('mxxn-icon');
+    await icon.updateComplete;
+    await icon.isLoading;
+
+    expect(icon.shadowRoot.innerHTML).toContain('11H21V13H3V11M3');
+
+    icon.name = 'minus';
+    await icon.updateComplete;
+    await icon.isLoading;
+
+    expect(icon.shadowRoot.innerHTML).toContain('13H5V11H19V13Z');
+  });
 
   afterEach(() => {
     document.body.innerHTML = '';
